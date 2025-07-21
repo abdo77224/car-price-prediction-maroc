abb18b87d1144e48d09aa801d003b7ac16bbd3b9
abdo <abdosafadi2003@gmail.com>
2025-07-21 22:53:57 +0100

    first commit

--- learn_scrapping.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-import re
-#importer le code de la page 
-url='https://www.avito.ma/fr/maroc/voitures_d_occasion-%C3%A0_vendre'
-reponse = requests.get(url)
-soup=BeautifulSoup(reponse.text, 'html.parser')
-print(soup) 
